e4221d15dd6b430d33c27d94da225465a820a69a
Christoph Ritler <christoph.ritler@garaio.com>
2012-03-13 14:16:28 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContextNavigation::Application.config.secret_token = '023ec8690235508e0bc36690aff6a28ba888033dd2c49e334e901a1530562b6e556c5560a704c75231687361673ed0b7d74c9c0306ea6016a0b6c6628856a050'
